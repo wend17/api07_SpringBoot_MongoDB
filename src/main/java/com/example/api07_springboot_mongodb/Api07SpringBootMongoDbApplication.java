e11b616e1899862549b121934eda89d026b3c27f
Wendy <wendyvillazana@gmail.com>
2023-01-25 17:04:37 -0500

    iniciando proyecto_properties

---
+++ src/main/java/com/example/api07_springboot_mongodb/Api07SpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.api07_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api07SpringBootMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api07SpringBootMongoDbApplication.class, args);
+    }
+
+}
